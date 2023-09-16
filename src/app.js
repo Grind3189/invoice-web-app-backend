edb428f51d98fb1e15543028e702d3146bb546ec
Grind <bellenlowell@gmail.com>
2023-09-16 20:07:29 +0800

    commit

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express()
 dotenv.config()
 
 const corsOptions = {
-    origin: ['https://invoice-by-grind.netlify.app', 'http://localhost:5173'],
+    origin: 'https://invoice-by-grind.netlify.app',
     methods: 'GET, POST, DELETE, PUT',        
     credentials: true  
 }
